e6d10d90842daeffc84d62565449d0fab5aad20d
Chalouati <chalouatimoemen@gmail.com>
2025-11-17 13:10:46 +0100

    dynamic and vs code stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 import { db } from "~/server/db";
 
+const mockUrls = [
+  "https://nojaa5ntrm.ufs.sh/f/q774PQV5VEswb0s9HEQX06DlCckLthAabznS1QTsmIx895yY",
+  "https://nojaa5ntrm.ufs.sh/f/q774PQV5VEswxFG6Jaqoalj0mWU3vkIqFTLy6szbiK4C9BQw",
+  "https://nojaa5ntrm.ufs.sh/f/q774PQV5VEswoZqx14b9Xw5lpjKef4okdJaZYV23cUN9v6gD",
+  "q774PQV5VEsw7OlCACQXV8tBl4ukMEcKAwz0RP93Tg2brSQj",
+];
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const posts = await db.query.posts.findMany();
   console.log(posts);
-  const mockUrls = [
-    "https://nojaa5ntrm.ufs.sh/f/q774PQV5VEswb0s9HEQX06DlCckLthAabznS1QTsmIx895yY",
-    "https://nojaa5ntrm.ufs.sh/f/q774PQV5VEswxFG6Jaqoalj0mWU3vkIqFTLy6szbiK4C9BQw",
-    "https://nojaa5ntrm.ufs.sh/f/q774PQV5VEswoZqx14b9Xw5lpjKef4okdJaZYV23cUN9v6gD",
-    "q774PQV5VEsw7OlCACQXV8tBl4ukMEcKAwz0RP93Tg2brSQj",
-  ];
-  const mockImages = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url,
-  }));
 
   return (
     <main className="">
